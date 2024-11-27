8ebaa03061549c9935d7247460d10d4a669194a2
diegorom27 <diegorom27@gmail.com>
2024-11-27 11:33:20 -0500

    Update vars.tf

--- terraform-infraestructura/vars.tf
+++ terraform-infraestructura/vars.tf
@@ -5,7 +5,7 @@
 variable ibm_region {
     description = "IBM Cloud region where all resources will be deployed"
     type        = string
-
+    default = "us-south"
     validation  {
       error_message = "Must use an IBM Cloud region. Use `ibmcloud regions` with the IBM Cloud CLI to see valid regions."
       condition     = can(
